a9e9c0ac32fa5ed406e996b1f3024d60e753c401
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-20 04:19:25 +0900

    now user's detail & edit page working

--- rhyme/urls.py
+++ rhyme/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rhyme import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('verses/', include('verses.urls')),
     path('', views.home, name='home'),
     path('rhymers/', include('users.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import User
+from verses.models import Verse
 from django.contrib.auth.decorators import login_required
 
 
 def detail(request, user_id):
     user = get_object_or_404(User, pk = user_id)
-    return render(request, 'users/detail.html', {'user':user})
+    posts = Verse.objects.filter(rhymer=user)
+    return render(request, 'users/detail.html', {'user':user, 'posts':posts})
 
 @login_required
 def edit(request, user_id):
     user = get_object_or_404(User, pk = user_id)
-    return render(request, 'users/edit.html', {'user':user})
+    if request.method == 'POST':
+        if request.POST['username']:
+            user.username = request.POST['username']
+        if request.POST['biography']:
+            user.biography = request.POST['biography']
+        if request.POST['email']:
+            user.email = request.POST['email']
+        if request.FILES['icon']:
+            user.icon = request.FILES['icon']
+        user.save()
+        return redirect('/rhymers/' + str(user_id))
+    else:
+        return render(request, 'users/edit.html', {'user':user})
