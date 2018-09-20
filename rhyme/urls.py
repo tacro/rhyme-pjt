c7bf9cdb77a4621729b83f82845caed44099135b
Tacro Hashimoto <tacro@g.ucla.edu>
2018-09-21 00:07:21 +0900

    added static pages

--- rhyme/urls.py
+++ rhyme/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('verses/', include('verses.urls')),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('help/', views.help, name='help'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
     path('rhymers/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- rhyme/views.py
+++ rhyme/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.contrib.auth.decorators import login_required
 import operator
 
 @login_required
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render_to_response('about.html')
+
+def help(request):
+    return render_to_response('help.html')
+
+def privacy(request):
+    return render_to_response('privacy.html')
+
+def terms(request):
+    return render_to_response('terms.html')
