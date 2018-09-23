2f063ffb17e76e1ab16864ea7995007fc5ca20b1
tacro <tacro@g.ucla.edu>
2018-09-23 14:21:31 +0000

    tweks

--- rhyme/settings.py
+++ rhyme/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zqew5lufs%h4gzmjro^&7#ozyh^xmm#a=ho()xz0apex0&)0=x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
